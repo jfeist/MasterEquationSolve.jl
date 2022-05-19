eccd5610a3c8554f3bdfd5c9babaa7c8252b1605
Johannes Feist <johannes.feist@gmail.com>
2022-05-19 16:03:08 +0200

    also add and run for input with e_ops in precompile.jl

--- precompile.jl
+++ precompile.jl
@@ -1,5 +1,8 @@
 using MasterEquationSolve
+using CUDA
 
 rm("precompile_outdir",force=true,recursive=true)
+CUDA.@time MasterEquationSolve.mesolve_qutip("precompile_input","precompile_outdir")
 
-MasterEquationSolve.mesolve_qutip("precompile_input","precompile_outdir")
+rm("precompile_outdir",force=true,recursive=true)
+CUDA.@time MasterEquationSolve.mesolve_qutip("precompile_input_eops","precompile_outdir")
