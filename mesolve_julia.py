2e103a0362787db14b4409121ab2cbc9f86dcc71
Johannes Feist <johannes.feist@gmail.com>
2022-06-14 00:04:50 +0200

    switch from string-based paths to pathlib.Path in mesolve_julia.py

--- mesolve_julia.py
+++ mesolve_julia.py
@@ -1,10 +1,10 @@
 import os, contextlib, subprocess
+from pathlib import Path
 from tempfile import mkdtemp
-from glob import glob
 from qutip import Qobj, qsave, qload
 import numpy as np
 
-MESOLVE_DIR = os.path.dirname(os.path.abspath(__file__))
+MESOLVE_DIR = Path(__file__).resolve().parent
 JOBFILE = "mesolve_julia.job"
 JOBIDFILE = "jobid.dat"
 JOBFILE_STR = f"""\
@@ -27,7 +27,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S'): finished"
 
 class mesolve_julia:
     """mesolve_julia is an object that allows solution of master
-    equation using the https://github.com/jfeist/MasterEquationSolve.jl julia
+    equations using the https://github.com/jfeist/MasterEquationSolve.jl julia
     library, which in particular makes use of the GPU to run significantly
     faster than on CPUs. There are two ways to create a mesolve_julia object:
 
@@ -60,37 +60,33 @@ class mesolve_julia:
     or expectation values are saved).
     """
     def __init__(self, H_or_dir, ρ0=None, ts=None, c_ops=None, e_ops="probabilities"):
-        if isinstance(H_or_dir,str):
-            dir = H_or_dir
-            self.dir = dir if dir.startswith("mesolve_julia_") or dir.startswith("./mesolve_julia_") else "./mesolve_julia_"+dir
+        if isinstance(H_or_dir,str) or isinstance(H_or_dir,Path):
+            dir = Path(H_or_dir)
+            self.dir = dir if dir.name.startswith("mesolve_julia_") else Path("mesolve_julia_"+str(dir))
             # this "with" statement suppresses unnecessary "loaded xy" statements from qload
             with open(os.devnull, 'w') as devnull:
                 with contextlib.redirect_stdout(devnull):
-                    self.data = qload(os.path.join(self.dir,"input"))
-            # [] to take the only element this should have - raises error if more than one file found
-            with open(os.path.join(self.dir,JOBIDFILE),"r") as f:
-                self.jobid = f.read()
+                    self.data = qload(str(self.dir/"input"))
+            self.jobid = (self.dir / JOBIDFILE).read_text()
         elif isinstance(H_or_dir,Qobj):
             if ρ0 is None or ts is None or c_ops is None:
                 raise ValueError(f"H_or_dir is a Qobj, but one of ρ0, ts, c_ops is None. They must have a value. Got ρ0 = {ρ0}, ts = {ts}, c_ops = {c_ops}.")
             self.data = (H_or_dir,c_ops,ρ0,ts,e_ops)
-            self.dir = mkdtemp(prefix="mesolve_julia_",dir=".")
-            qsave(self.data,os.path.join(self.dir,"input"))
-            with open(os.path.join(self.dir,JOBFILE),"w") as f:
-                f.write(JOBFILE_STR)
+            self.dir = Path(mkdtemp(prefix="mesolve_julia_",dir="."))
+            qsave(self.data, str(self.dir/"input"))
+            (self.dir/JOBFILE).write_text(JOBFILE_STR)
             out = subprocess.run(["sbatch",JOBFILE], cwd=self.dir, check=True, capture_output=True, text=True)
             self.jobid = out.stdout.strip().replace("Submitted batch job ","")
-            with open(os.path.join(self.dir,JOBIDFILE),"w") as f:
-                f.write(self.jobid)
+            (self.dir/JOBIDFILE).write_text(self.jobid)
             print(out.stdout.strip(),"with data in",self.dir)
         else:
             raise ValueError(f"H_or_dir must be either a string or a Qobj, got: {H_or_dir}")
             
     def load_results(self):
-        resfis = sorted(glob(os.path.join(self.dir,"[0-9]*.qu")))
+        resfis = sorted(self.dir.glob("[0-9]*.qu"))
         # these "with" statements suppress unnecessary "loaded xy" statements from qload
         with open(os.devnull, 'w') as devnull:
             with contextlib.redirect_stdout(devnull):
-                tPs = [qload(f.replace(".qu","")) for f in resfis]
+                tPs = [qload(str(f).replace(".qu","")) for f in resfis]
         self.times = np.array([t for (t,P) in tPs])
         self.Ps = np.array([P for (t,P) in tPs])
