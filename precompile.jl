00984b11a9560ab940bafe0f0e1036e6d60d8715
Johannes Feist <johannes.feist@gmail.com>
2022-06-14 00:56:39 +0200

    update precompile files for new sesolve

--- precompile.jl
+++ precompile.jl
@@ -1,8 +1,12 @@
 using MasterEquationSolve
 using CUDA
 
-rm("precompile_outdir",force=true,recursive=true)
-CUDA.@time MasterEquationSolve.mesolve_qutip("precompile_input","precompile_outdir")
+function dorun(func,suff)
+    rm("precompile_outdir",force=true,recursive=true)
+    CUDA.@time func("precompile_input$(suff)","precompile_outdir")
+end
 
-rm("precompile_outdir",force=true,recursive=true)
-CUDA.@time MasterEquationSolve.mesolve_qutip("precompile_input_eops","precompile_outdir")
+dorun(mesolve_qutip,"")
+dorun(mesolve_qutip,"_eops")
+dorun(sesolve_qutip,"_se")
+dorun(sesolve_qutip,"_se_eops")
